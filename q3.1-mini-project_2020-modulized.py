e315237355e4f978ff97c46a178ed596e208b1b2
tim010007 <tim010007@gmail.com>
2020-04-21 17:03:14 +0200

    trial modulized version: minor bug resolved

--- q3.1-mini-project_2020-modulized.py
+++ q3.1-mini-project_2020-modulized.py
@@ -6,73 +6,97 @@ class model:
         self.s = [None]
         self.w = [None]
         self.b = [None]
-        self.dl_dw = []
-        self.dl_db = []
-        self.dl_ds = []
-        self.dl_dx = []
+        self.dl_dw = [None]
+        self.dl_db = [None]
+        self.dl_ds = [None]
+        self.dl_dx = [None]
         self.lt = 0
         self.ln = 0
-        
+        self.ini = 0
     def loss(self,x,t):
-        return (x - t).pow(2).sum()
-    def dloss(delf,x,t):
+        return (x-t).pow(2).sum()
+    def dloss(self,x,t):
         return 2*(x-t)
     def l_counter(self):
         lt = self.lt
         self.lt = lt + 1
-
-    def train(self,x,t,*argv):
-        for i in range(len(argv)):
-            argv[i].forward(x, self.ln)
-        for i in range(len(argv)):
-            argv[i].backward(t, self.ln)
-            
-class optimizer:
+    def sequential(self,x,t,*argv):
+        if self.ini == 0:
+            for i in range(len(argv)):
+                argv[i].ini(x)
+            for i in range(len(argv)):
+                argv[i].forward(x, self.ln)
+            for i in range(len(argv)-1,-1,-1):
+                argv[i].backward(t, self.ln)
+            self.ini = 1
+        elif self.ini == 1:
+            for i in range(len(argv)):
+                argv[i].forward(x, self.ln)
+            for i in range(len(argv)-1,-1,-1):
+                argv[i].backward(t, self.ln)            
+                 
+class criterion_mse:
+    def __init__(self,nn):
+        return None
+    def loss(self,x,t):
+        return (x - t).pow(2).sum()
+    def dloss(delf,x,t):
+        return 2*(x-t)
+                    
+class optimizer_sgd:
     def __init__(self,lr,nn):
         self.lr = lr
-    
-    def sgd(self):
-        for i in range(lt):
-            nn.w[i] = nn.w[i] - lr*nn.dl_dw[i]
-            nn.b[i] = nn.b[i] - lr*nn.dl_db[i]
-        
+        self.nn = nn
+    def update(self):
+        for i in range(1,nn.lt):
+            self.nn.w[i] = self.nn.w[i] - lr*self.nn.dl_dw[i]
+            self.nn.b[i] = self.nn.b[i] - lr*self.nn.dl_db[i]
+
 class sigmoid:
     def __init__(self, nn):
+        self.nn = nn
         return None
+    def ini(self,x):
+        pass
     def forward(self,x,ln):
-        s0 = s[ln]
-        nn.x[ln] = s0.tanh()
+        s0 = self.nn.s[ln]
+        self.nn.x[ln] = s0.tanh()
     def backward(self,t,ln):
-        s0 = nn.s[ln]
-        nn.dl_ds[ln] = 4*(s0.exp() + s0.mul(-1).exp()).pow(-2)
+        s0 = self.nn.s[ln]
+        self.nn.dl_ds[ln] = 4*(s0.exp() + s0.mul(-1).exp()).pow(-2)
 
-class Linear:
-    def __init__(self, m, n, nn):
-        self.epsilon = 1e-1 
-        w = torch.empty(m, n).normal_(0, self.epsilon)
-        b = torch.empty(m).normal_(0, self.epsilon)
-        nn.x.append( None )
-        nn.s.append( None )
-        nn.w.append( w )
-        nn.b.append( b )
-        nn.dl_dw.append( torch.empty(w.size()) )
-        nn.dl_db.append( torch.empty(b.size()) ) 
-        nn.dl_ds.append( None )
-        nn.dl_dx.append( None )
-        nn.x
-        nn.l_counter()
-        
+class linear:
+    def __init__(self, m, n, nn, criterion):
+        self.epsilon = 1e-1
+        self.n = n
+        self.m = m
+        self.nn = nn
+        self.cri = criterion
+    def ini(self,x):
+        w = torch.empty(self.n, self.m).normal_(0, self.epsilon)
+        b = torch.empty(self.n).normal_(0, self.epsilon)
+        self.nn.x.append( None )
+        self.nn.s.append( None )
+        self.nn.w.append( w )
+        self.nn.b.append( b )
+        self.nn.dl_dw.append( torch.empty(w.size()) )
+        self.nn.dl_db.append( torch.empty(b.size()) )
+        self.nn.dl_ds.append( torch.empty(self.n).fill_(1.0) )
+        self.nn.dl_dx.append( torch.empty(self.n).fill_(1.0) )
+        self.nn.x
+        self.nn.l_counter()        
     def forward(self,x,ln):
-        nn.x[0] = x
-        nn.s[ln+1] = nn.w[ln+1].mv(nn.x[ln]) + nn.b[ln+1]
-        ln = ln + 1
+        self.nn.x[0] = x
+        self.nn.x[ln+1] = self.nn.w[ln+1].mv(self.nn.x[ln]) + self.nn.b[ln+1]
+        self.nn.s[ln+1] = self.nn.x[ln+1]
+        self.nn.ln = ln + 1
     def backward(self,t,ln):
-        if ln==nn.lt:
-            nn.dl_dx[ln] = nn.dloss(nn.x[ln], t)            
-            nn.dl_ds[ln] = nn.dl_ds[ln] * nn.dl_dx[ln]
-        elif ln!=nn.lt:
-            nn.dl_dx[ln] = nn.w[ln+1].t().mv(nn.dl_ds[ln+1])
-            nn.dl_ds[ln] = nn.dl_ds[ln] * nn.dl_dx[ln]               
-        nn.dl_dw[ln] = nn.dl_ds[ln].view(-1, 1).mm(nn.x[ln-1].view(1, -1))
-        nn.dl_db[ln] = nn.dl_ds[ln]
-        ln = ln - 1
+        if ln==self.nn.lt:
+            self.nn.dl_dx[ln] = self.cri.dloss(self.nn.x[ln], t)            
+            self.nn.dl_ds[ln] = self.nn.dl_ds[ln] * self.nn.dl_dx[ln]
+        elif ln!=self.nn.lt:
+            self.nn.dl_dx[ln] = self.nn.w[ln+1].t().mv(self.nn.dl_ds[ln+1])
+            self.nn.dl_ds[ln] = self.nn.dl_ds[ln] * self.nn.dl_dx[ln]               
+        self.nn.dl_dw[ln] = self.nn.dl_ds[ln].view(-1, 1).mm(self.nn.x[ln-1].view(1, -1))
+        self.nn.dl_db[ln] = self.nn.dl_ds[ln]
+        self.nn.ln = ln - 1
